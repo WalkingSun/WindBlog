f8b82815c01c9872f07f98d02d375be3b2b21794
sunchenwei <sun.chenwei@puscene.com>
2018-12-23 20:41:45 +0800

    支持用户登录

--- models/ArtitleJuejin.php
+++ models/ArtitleJuejin.php
@@ -9,12 +9,49 @@
 namespace app\models;
 
 
+use PHPHtmlParser\Dom;
+
 class ArtitleJuejin implements Article
 {
-    public function lists( $data ){
+    public function lists( $data=[] ){
+        $result = [];
+        $tag = !empty($data['tag'])?$data['tag']:'php';
+        $dom = new Dom();
+        $url = "https://segmentfault.com/t/{$tag}/blogs";
+        $html = Common::httpGet($url);
+        $dom->load($html);
+        $info = $dom->find('.summary');
+        if( $info ){
+
+        }
+
+
+        return $result;
+    }
+
+    public function analysis( $data=[] ){
         $result = [];
 
+        return $result;
+    }
 
+    /**
+     * 获取标签
+     */
+    public function getTag(){
+        $result = [];
+        $tagUrl = 'https://segmentfault.com/tags/all';
+        $html = Common::httpGet($tagUrl);
+
+        $dom = new Dom();
+        $dom->load($html);
+
+        $list = $dom->find('.tag-list__item');
+        if($list){
+            foreach ($list as $v){
+                $result[] = $v.find('.h4').text;
+            }
+        }
         return $result;
     }
 }
